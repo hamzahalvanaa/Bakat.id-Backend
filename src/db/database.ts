e71bbddeb51bc819090c0be8d0f589ac03c5a96e
Sergi <snak_one@hotmail.com>
2019-05-07 08:50:11 +0200

    Update database.ts

--- src/db/database.ts
+++ src/db/database.ts
@@ -4,7 +4,7 @@ mongoose.set('useCreateIndex', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useNewUrlParser', true);
 
-const URI = 'mongodb+srv://MONGO_USER:MONGOPASSWORD@CLUSTER_URL/DB_NAME?retryWrites=true';
+const URI = 'mongodb+srv://MONGO_USER:MONGO_PASSWORD@CLUSTER_URL/DB_NAME?retryWrites=true';
 
 mongoose.connect(URI)
   .then(db => console.log('Mongo Cloud Status: \x1b[35m%s\x1b[0m', 'OK'))
